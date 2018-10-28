c6a7d371087b2d9c292de3e963595df88f8e00c4
Leah Meromy <lmeromy@gmail.com>
2018-10-28 16:03:23 +0000

    Test branch commit

--- client/src/views/water_use_input.js
+++ client/src/views/water_use_input.js
@@ -6,28 +6,28 @@ const WaterUseInputView = function (form) {
 
 WaterUseInputView.prototype.bindEvents = function () {
    this.form.addEventListener('submit', (event) => {
-     console.log('from input view:', event);
+     // console.log('from input view:', event);
      this.handleSubmit(event);
    });
 };
 
 WaterUseInputView.prototype.handleSubmit = function (evt) {
   evt.preventDefault();
-  // const newWaterUser = this.createWaterUser(evt.target);
+  const newWaterUser = this.createWaterUser(evt.target);
   PubSub.publish('WaterUseInputView:water-user-submit', newWaterUser);
   evt.target.reset;
 };
 
-//
-// WaterUseInputView.prototype.createWaterUser = function (input) {
-//   const newWaterUser = {
-//     name: "inputUser",
-//     teeth: input.teeth.value,
-//     flush: input.flush.value,
-//     bathe: input.bathe.value
-//   }
-//   return newWaterUser;
-// };
+
+WaterUseInputView.prototype.createWaterUser = function (input) {
+  const newWaterUser = {
+    name: "inputUser",
+    teeth: input[0].value,
+    flush: input[1].value,
+    bathe: input[2].value
+  }
+  return newWaterUser;
+};
 
 
 module.exports = WaterUseInputView;
