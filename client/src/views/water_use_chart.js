785f630b6eb8dea4656b8a0d113226daaad932e0
Anna Henderson <annahenderson@gmail.com>
2018-10-31 10:08:22 +0000

    submit form and charts colours adjusted

--- client/src/views/water_use_chart.js
+++ client/src/views/water_use_chart.js
@@ -43,83 +43,86 @@ WaterUseChart.prototype.prepareInvisWaterUse = function (data) {
 WaterUseChart.prototype.renderVis = function (chartArray) {
 
   google.charts.load("current", {packages:['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        const data = google.visualization.arrayToDataTable([
-          ["Average Water Use", "Your Water Use", { role: "style" } ],
-          ["Average Water Use", chartArray[0], "lightblue"],
-          ["Your Water Use", chartArray[1], "blue"]
-        ]);
+  google.charts.setOnLoadCallback(drawChart);
+  function drawChart() {
+    const data = google.visualization.arrayToDataTable([
+      ["Average Water Use", "Your Water Use", { role: "style" } ],
+      ["Average Water Use", chartArray[0], "green"],
+      ["Your Water Use", chartArray[1], "blue"]
+    ]);
 
-        const view = new google.visualization.DataView(data);
-        view.setColumns([0, 1, 2]);
+    const view = new google.visualization.DataView(data);
+    view.setColumns([0, 1, 2]);
 
-        const options = {
-          title: "Visible Water Use: This is the water we get from our kitchen and bathroom sinks for drinking, brushing our teeth, washing ourselves, and cleaning our house.",
-          width: 350,
-          height: 400,
-          vAxis: {
-            title: 'Litres of Water per day',
-            minValue: 100,
-            maxValue: 170,
-            baselineColor: '#DDD',
-            gridlines: {
-              color: '#DDD',
-              count: 6
-            }
-          },
-          backgroundColor: {
-       fill: '#FFBA08'
-     },
-          bar: {groupWidth: "95%"},
-          legend: { position: "none" },
-        };
-        const chart = new google.visualization.ColumnChart(document.getElementById("water-use-vis-chart"));
-        chart.draw(view, options);
+    const options = {
+      title: "Visible Water Use: This is the water we get from our kitchen and bathroom sinks for drinking, brushing our teeth, washing ourselves, and cleaning our house.",
+      width: 350,
+      height: 400,
+      vAxis: {
+        title: 'Litres of Water per day',
+        minValue: 100,
+        maxValue: 170,
+        baselineColor: '#DDD',
+        gridlines: {
+          color: '#DDD',
+          count: 6
+        }
+      },
+      backgroundColor: {
+        fill: '#BBDEF0'
+      },
+      bar: {groupWidth: "95%"},
+      legend: { position: "none" },
+    };
+    const chart = new google.visualization.ColumnChart(document.getElementById("water-use-vis-chart"));
+    chart.draw(view, options);
 
     //     $(window).resize(function(){
     //       drawChart();
-    }
+  }
 
 };
 
 WaterUseChart.prototype.renderInvis = function (chartArray) {
 
   google.charts.load("current", {packages:['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        const data = google.visualization.arrayToDataTable([
-          ["Average UK Meat Diet", "Average UK Vegetarian Diet", { role: "style" } ],
-          ["Average UK Meat Diet", chartArray[0], "red"],
-          ["Average UK Vegetarian Diet", chartArray[1], "yellow"],
-          ["Average UK Vegan Diet", chartArray[2], "green"],
-          ["Your Diet", chartArray[3], "blue"]
+  google.charts.setOnLoadCallback(drawChart);
+  function drawChart() {
+    const data = google.visualization.arrayToDataTable([
+      ["Average UK Meat Diet", "Average UK Vegetarian Diet", { role: "style" } ],
+      ["Average UK Meat Diet", chartArray[0], "red"],
+      ["Average UK Vegetarian Diet", chartArray[1], "yellow"],
+      ["Average UK Vegan Diet", chartArray[2], "green"],
+      ["Your Diet", chartArray[3], "blue"]
 
-        ]);
+    ]);
 
-        const view = new google.visualization.DataView(data);
-        view.setColumns([0, 1, 2]);
+    const view = new google.visualization.DataView(data);
+    view.setColumns([0, 1, 2]);
 
-        const options = {
-          title: "Invisible Water Use: This is the water we do not 'see', but we need it in order to grow the all food we eat and to make the items we use every day (including things like clothes, toys, and computers).",
-          width: 600,
-          height: 400,
-          vAxis: {
-            title: 'Litres of Water per day',
-            minValue: 0,
-            maxValue: 4500,
-            baselineColor: '#DDD',
-            gridlines: {
-              color: '#DDD',
-              count: 6
-            }
-          },
-          bar: {groupWidth: "95%"},
-          legend: { position: "none" },
-        };
-        const chart = new google.visualization.ColumnChart(document.getElementById("water-use-invis-chart"));
-        chart.draw(view, options);
-    }
+    const options = {
+      title: "Invisible Water Use: This is the water we do not 'see', but we need it in order to grow the all food we eat and to make the items we use every day (including things like clothes, toys, and computers).",
+      width: 600,
+      height: 400,
+      vAxis: {
+        title: 'Litres of Water per day',
+        minValue: 0,
+        maxValue: 4500,
+        baselineColor: '#DDD',
+        gridlines: {
+          color: '#DDD',
+          count: 6
+        }
+      },
+      backgroundColor: {
+        fill: '#BBDEF0'
+      },
+      bar: {groupWidth: "95%"},
+      legend: { position: "none" },
+    };
+    const chart = new google.visualization.ColumnChart(document.getElementById("water-use-invis-chart"));
+    chart.draw(view, options);
+  }
 
 };
 
